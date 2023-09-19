01371acbdaaf983e946894d5af2545ea659dc1b9
Hugo Slabbert <hugo@slabnet.com>
2023-09-18 21:29:21 -0700

    move_semantics2, option2: simple clone in function args

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -11,14 +11,14 @@
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let vec1 = fill_vec(&vec0);
+    let vec1 = fill_vec(vec0.clone());
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
-fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
-    let mut vec = vec.clone();
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
 
     vec.push(88);
 

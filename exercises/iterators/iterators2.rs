91e4cf617a15d3820a2d1524f859739b2a2f33e9
Hugo Slabbert <hugo@slabnet.com>
2024-01-05 22:10:06 -0800

    iterators2, explicit

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -15,7 +15,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().to_string() + c.as_str(),
     }
 }
 
@@ -24,7 +24,10 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words
+        .into_iter()
+        .map(|&x| capitalize_first(x))
+        .collect::<Vec<String>>()
 }
 
 // Step 3.
@@ -32,7 +35,10 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words
+        .into_iter()
+        .map(|&x| capitalize_first(x))
+        .collect::<String>()
 }
 
 #[cfg(test)]
